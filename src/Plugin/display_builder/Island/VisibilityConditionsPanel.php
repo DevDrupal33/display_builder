9c5bf1069b8e7c3dfe3316f3179b66cb2f519008
yannickoo <drupal@yannick.md>
2025-12-15 05:47:48 +0100

    [#3563124] - hides 'current_theme' condition plugin in visibility conditions

--- src/Plugin/display_builder/Island/VisibilityConditionsPanel.php
+++ src/Plugin/display_builder/Island/VisibilityConditionsPanel.php
@@ -56,9 +56,13 @@ class VisibilityConditionsPanel extends IslandPluginBase implements IslandWithFo
     unset($conditions['response_status']);
 
     foreach ($conditions as $condition_id => $definition) {
+      if ($condition_id === 'current_theme') {
+        continue;
+      }
       if (\str_starts_with($condition_id, 'entity_bundle:')) {
         continue;
       }
+
       /** @var \Drupal\Core\Condition\ConditionInterface $condition */
       $condition = $this->conditionManager->createInstance($condition_id, $instance[$condition_id] ?? []);
       $form_state->set(['conditions', $condition_id], $condition);
