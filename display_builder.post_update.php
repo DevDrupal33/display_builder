c22032ab83a0ea2bf84f0ac4adc338ac2d8d3834
Jean Valverde <moimog33@gmail.com>
2026-03-11 17:44:56 +0100

    chore(instance): update for content type change of instance

---
+++ display_builder.post_update.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * @file
+ * Contains post update functions for the Display Builder module.
+ *
+ * @phpcs:disable Generic.CodeAnalysis.UnusedFunctionParameter.Found
+ */
+
+declare(strict_types=1);
+
+/**
+ * Delete all states after ContentEntityType migration.
+ *
+ * @param array $sandbox
+ *   An associative array containing information about the progress of the
+ *   update.
+ */
+function display_builder_post_update_1(&$sandbox): void {
+  $storage = Drupal::service('entity_type.manager')->getStorage('display_builder_instance');
+  $instances = $storage->loadMultiple();
+  $storage->delete($instances);
+
+  Drupal::service('plugin.cache_clearer')->clearCachedDefinitions();
+}
